7a7623d7f2553a70d71076a3456006d84bc96bf5
Igor Hey Matos <iheymatos1@gmail.com>
2024-03-24 17:52:30 -0300

    Exercício05

---
+++ Exerc04/Conversao.java
@@ -0,0 +1,10 @@
+package Exerc04;
+
+public class Conversao {
+    public float dolarDisp;
+    public float cotacaoDolar;
+
+    public float valorReal() {
+        return dolarDisp * cotacaoDolar;
+    }
+}
--- Programa.java
+++ Programa.java
@@ -2,6 +2,7 @@ public class Programa {
     public static void main(String[] args) {
         // Exerc01.Exercicio01.executar();
         // Exerc02.Exercicio02.executar();
-        Exerc03.Exercicio03.executar();
+        //Exerc03.Exercicio03.executar();
+        Exerc04.Exercicio04.executar();
     }
 }
